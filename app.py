9ad871cfef994683eb032cb979a1a6b060cb5125
Ganesh V Magham <67683339+riphunter618@users.noreply.github.com>
2025-09-01 09:51:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -46,6 +46,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,  # or restrict to ["http://localhost:5500"] etc.
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
@@ -196,6 +197,7 @@ def test33(data: ImageData):
     except Exception as e:
         logging.info(f'error is {e}')
         return {"status": "error", "message": f"Error: {str(e)}"}
+
 
 
 
